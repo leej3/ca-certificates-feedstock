816e04d0a3189751bf1acb6bd9657cafeda9e054
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-06-26 12:09:59 -0400

    recipe: Set/unset environment variables to point to certificates.

---
+++ recipe/activate.sh
@@ -0,0 +1,2 @@
+export SSL_CERT_DIR="${CONDA_ENV_PATH}/ssl"
+export SSL_CERT_FILE="${SSL_CERT_DIR}/cacert.pem"
--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
+setlocal EnableDelayedExpansion
+
 :: Create the directory to hold the certificates.
 if not exist %LIBRARY_PREFIX%\ssl mkdir %LIBRARY_PREFIX%\ssl
 if errorlevel 1 exit 1
@@ -7,3 +9,10 @@ copy /y %SP_DIR%\certifi\cacert.pem %LIBRARY_PREFIX%\ssl
 if errorlevel 1 exit 1
 copy /y %LIBRARY_PREFIX%\ssl\cacert.pem %LIBRARY_PREFIX%\ssl\cert.pem
 if errorlevel 1 exit 1
+
+:: Copy the [de]activate scripts to %LIBRARY_PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+FOR %%F IN (activate deactivate) DO (
+    IF NOT EXIST %LIBRARY_PREFIX%\etc\conda\%%F.d MKDIR %LIBRARY_PREFIX%\etc\conda\%%F.d
+    COPY %RECIPE_DIR%/%%F.bat %LIBRARY_PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+)
